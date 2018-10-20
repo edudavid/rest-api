5cfc1855808fa64ff17ef1f46ff2aca085f5b2ac
Eduardo David <edudavid@gmail.com>
2018-10-20 18:13:02 -0300

    Initial commit

--- api/controllers/LeadsController.js
+++ api/controllers/LeadsController.js
@@ -1,47 +1,6 @@
 const Lead = require('../models/Lead')
 const { buildQuery } = require('../utils/QueryBuilder')
-/**
- * @swagger
- * /leads:
- * 
- *   get:
- *     description: "Returns all leads"
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: q
- *         description: "The query parameter. The format is attribute:value. For multiple attributes and values use: attribute1:value1,attribute2:value2 "
- *         in:  query
- *         required: false
- *         type: string
- *       - name: fields
- *         description: "The document fields you want to have in the returned documents. Ex: 'name,email'"
- *         in:  query
- *         required: false
- *         type: string
- *       - name: page
- *         description: "Page number"
- *         in:  query
- *         required: false
- *         type: integer
- *       - name: limit
- *         description: "Maximum documents per page"
- *         in:  query
- *         required: false
- *         type: string
- *       - name: sort
- *         description: "Sorting attribute. By default is asc for desc use - before attribute name. Ex: name for asc and -name for desc"
- *         in:  query
- *         required: false
- *         type: string
- *     responses:
- *       200:
- *         description: "successful operation"
- *         schema:
- *           type: array
- *           items:
- *             $ref: '#/definitions/Lead'
- */ 
+
 exports.get = (req, res, next) => {
   const page = parseInt(req.query.page || 1)
   const limit = parseInt(req.query.limit || 10)
@@ -56,103 +15,27 @@ exports.get = (req, res, next) => {
   });
 };
 
-/**
- * @swagger
- * /leads:
- * 
- *   post:
- *     description: "Creates a lead"
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: lead
- *         description: "Lead object"
- *         in:  body
- *         required: true
- *         type: string
- *         schema:
- *           $ref: '#/definitions/Lead'
- *     responses:
- *       200:
- *         description: "Creates a lead"
- *       400:
- *         description: "An error was fount on the request payload or the payload is invalid"
- */
 exports.create = (req, res, next) => {
   const lead = new Lead(req.body);
   lead.save((err, lead) => {
     if (err) { return next(err) }
 
-    res.json(lead)
+    res.location(`leads/${lead._id}`)
+    res.status(201).json(lead)
   });
 };
 
-/**
- * @swagger
- * /leads/{id}:
- * 
- *   get:
- *     description: "Returns a lead by id"
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: id
- *         description: "Lead id"
- *         in:  path
- *         required: true
- *         type: string
- *     responses:
- *       200:
- *         description: "successful operation"
- *         schema:
- *           type: object
- *           $ref: '#/definitions/Lead'
- *       404:
- *         description: "Lead not found"
- *       400:
- *         description: "Invalid ID supplied"
- */
 exports.findById = (req, res, next) => {
   Lead.findById(req.params.id, (err, lead) => {
     if (err) { return next(err) }
     if (!lead) { return next() }
     
+    res.setHeader('Cache-Control', 'public, max-age=3000');
     res.json(lead)
   });
 };
 
-/**
- * @swagger
- * /leads/{id}:
- * 
- *   put:
- *     description: "Updates a lead by id"
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: id
- *         description: "Lead id"
- *         in:  path
- *         required: true
- *         type: string
- *       - name: lead
- *         description: "Lead object"
- *         in:  body
- *         required: true
- *         type: string
- *         schema:
- *           $ref: '#/definitions/Lead'
- *     responses:
- *       200:
- *         description: "successful operation"
- *         schema:
- *           type: object
- *           $ref: '#/definitions/Lead'
- *       404:
- *         description: "Lead not found"
- *       400:
- *         description: "Invalid ID supplied"
- */ 
+
 exports.update = (req, res, next) => {
   Lead.findOneAndUpdate(
     { _id: req.params.id}, req.body, { runValidators: true, new: true }, (err, lead) => {
@@ -163,28 +46,7 @@ exports.update = (req, res, next) => {
   });
 };
 
-/**
- * @swagger
- * /leads/{id}:
- * 
- *   delete:
- *     description: "Deletes a lead by id"
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: id
- *         description: "Lead id"
- *         in:  path
- *         required: true
- *         type: string
- *     responses:
- *       200:
- *         description: "Lead successfully deleted"
- *       404:
- *         description: "Lead not found"
- *       400:
- *         description: "Invalid ID supplied"
- */
+
 exports.delete = (req, res, next) => {
   Lead.findOneAndRemove({ _id: req.params.id }, (err, lead) => {
     if (err) { return next(err) }
--- server.js
+++ server.js
@@ -3,43 +3,19 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const ErrorHandler = require('./api/middlewares/ErrorHandler')
 const routes = require('./api/routes/LeadsRoutes')
-const swaggerJSDoc = require('swagger-jsdoc')
-const swaggerUi = require('swagger-ui-express');
-
-const swaggerSpec = swaggerJSDoc({
-    swaggerDefinition: {
-      info: {
-        title: 'Leads REST API',
-        version: '1.0.0',
-      },
-    },
-    apis: ['./api/controllers/*.js', './api/models/*.js'],
-  }
-);
-
 
 const app = express()
 const port = process.env.PORT || 3000
 
-
 mongoose.Promise = global.Promise;
-mongoose.connect('MONGO_URL')
+mongoose.connect('mongodb+srv://rest-api:To7naqWAkNG7E1zC@cluster0-kh4jw.mongodb.net/test?retryWrites=true')
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
 routes(app);
 
 app.use(ErrorHandler.handleError);
-app.get('/api-docs.json', function(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(swaggerSpec);
-});
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(null, {
-    swaggerUrl: `http://localhost:${port}/api-docs.json`
-}));
 
 app.listen(port)
 console.log('leads REST API started on: ' + port);
