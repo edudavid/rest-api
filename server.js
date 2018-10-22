33ee398a3f76db782bfe4b71c92820405844bc7c
Eduardo David <edudavid@gmail.com>
2018-10-22 11:09:47 -0300

    Fixing middleware

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const ErrorHandler = require('./api/middlewares/ErrorHandler');
 const routes = require('./api/routes/LeadsRoutes');
 
 const app = express();
@@ -14,7 +13,5 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 routes(app);
-
-app.use(ErrorHandler.handleError);
 
 app.listen(port);
